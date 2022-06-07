77993bc3a71a76017c9301c33a1abf749bb132b3
Szymon Gackowski <szymon.gackowski@callpage.io>
2022-06-07 18:02:23 +0200

    store encrypted files

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FileUploadRequest;
 use App\Models\File;
 use App\Models\Transfer;
-use Brainstud\FileVault\Facades\FileVault;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class FilesController extends Controller
 {
     public function store(Transfer $transfer, FileUploadRequest $request)
     {
-        $filename = $request->file('file')->store('files');
-        FileVault::encrypt($filename);
+        $uploadedFile = $request->file('file');
+        $filename = Str::random() . '.enc';
+        $result = Storage::disk('s3')->put($filename, Crypt::encrypt($uploadedFile->getContent()));
 
-        /** @var File $file */
-        $file = $transfer->files()->create([
-            'location' => $filename,
-            'name'     => $request->file('file')->getClientOriginalName(),
-        ]);
+        if ($result === true) {
+            /** @var File $file */
+            $file = $transfer->files()->create([
+                'location' => $filename,
+                'name'     => $request->file('file')->getClientOriginalName(),
+            ]);
+        }
         unlink($request->file('file')->getRealPath());
 
         return response()->json([
